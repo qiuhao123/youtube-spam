135aeba0085b45479fd7f7d50d0a0b4652b90e67
Qiuhao Jin <jinqiuhao@gmail.com>
2020-02-02 17:56:53 -0500

    working app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # youtube-spam
+The objective of this project is to create continous delivery web app that helps user to classify spam comments on youtube. 
+The data set is collected from UCI Machine Learning Repository. It is a public set of comments collected for spam research. 
+It has five datasets composed by 1,956 real messages extracted from five videos that were among the 10 most viewed on the collection period.
--- main.py
+++ main.py
@@ -6,51 +6,29 @@ from flask import request
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
-# # @app.route('/table')
-# # def display():
-# if __name__ == 'main':
-#     app.run(host="127.0.0.1",port=8080,debug=True)
 @app.route('/')
 def hello():
     return render_template("home.html")
 
 @app.route('/',methods=['POST'])
 def predict():
-    url = "https://github.com/qiuhao123/youtube-spam/blob/master/youtube_spam.csv"
-    df = pd.read_csv(url)
-    df_data = df[["CONTENT", "CLASS"]]
-    # Features and Labels
-    df_x = df_data['CONTENT']
-    df_y = df_data.CLASS
-    # Extract Feature With CountVectorizer
-    corpus = df_x
+    df = pd.read_csv("youtube_spam.csv",header=0)
+    feature = df['CONTENT']
+    y = df.CLASS
     cv = CountVectorizer()
-    X = cv.fit_transform(corpus) # Fit the Data
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, df_y, test_size=0.33, random_state=42)
-    #Naive Bayes Classifier
-    from sklearn.naive_bayes import MultinomialNB
-    clf = MultinomialNB()
-    clf.fit(X_train,y_train)
-    clf.score(X_test,y_test)
-    #Alternative Usage of Saved Model
-    # ytb_model = open("naivebayes_spam_model.pkl","rb")
-    # clf = joblib.load(ytb_model)
-    if request.method == 'POST':
-        comment = request.form['comment']
-        data = [comment]
-        vect = cv.transform(data).toarray()
-        my_prediction = clf.predict(vect)
-        return render_template("response.html", prediction = my_prediction, comment = comment)
-    #return render_template('response.html',prediction=prediction,)
-
+    X = cv.fit_transform(feature)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    model = MultinomialNB()
+    model.fit(X_train, y_train)
+    if request.method == "POST":
+        comment = request.form['message']
+        x_pred = [comment]
+        transform = cv.transform(x_pred)
+        y_pred = model.predict(transform)
+        return render_template("response.html", prediction = y_pred)
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
